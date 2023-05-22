633725e1706ac7c2182d4ae581345d88dc315802
Lcheck <znfhdl789@naver.com>
2023-05-22 19:05:05 +0900

    Add PostEntity

--- src/main/java/ium/pethub/domain/entity/Post.java
+++ src/main/java/ium/pethub/domain/entity/Post.java
@@ -1,26 +1,68 @@
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(nullable = false, name = "post_id")
+//    private Long id;
+//
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+//
+
+
+
 package ium.pethub.domain.entity;
+//DB의 테이블 역할을 하는 클래스
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+        import ium.pethub.dto.post.request.PostRequestDto;
+        import ium.pethub.dto.post.request.PostRequestDto;
+        import lombok.Getter;
+        import lombok.Setter;
 
-import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
-
+        import javax.persistence.*;
 @Entity
-@Setter
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "posts")
-public class Post {
+@Setter
+@Table(name= "post_table")
+
+public class Post extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false, name = "post_id")
     private Long id;
+//    @Column(length = 20,nullable = false)
+//    private String postWriter;
+    @Column
+    private String postTitle;
+//    @Column
+//    private Integer postHits;
 
+    @Column(length =500)
+    private String postContents;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+
+    public static Post toSaveEntity(PostRequestDto postRequestDto){
+
+        Post postEntity = new Post();
+//        postEntity.setPostWriter(postRequestDto.getPostWriter());
+        postEntity.setPostTitle(postRequestDto.getPostTitle());
+        postEntity.setPostContents(postRequestDto.getPostContents());
+//        postEntity.setPostHits(0);
+        return postEntity;
+    }
+
+    public static Post toUpdateEntity(PostRequestDto postRequestDto){
+
+        Post postEntity = new Post();
+//        postEntity.setId(postRequestDto.getId()); //아이디 항목 추가
+//        postEntity.setPostWriter(postRequestDto.getPostWriter());
+        postEntity.setPostTitle(postRequestDto.getPostTitle());
+        postEntity.setPostContents(postRequestDto.getPostContents());
+//        postEntity.setPostHits(postRequestDto.getPostHits()); //조회수 유지용
+        return postEntity;
+    }
+
 }
+
